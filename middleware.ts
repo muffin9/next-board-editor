81262390f2e5f6b368f82bc039b30c5a62e0486f
muffin9 <jinha6707@gmail.com>
2024-11-27 14:46:27 +0900

    🍀 refactor: middleware file update url

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,23 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 // 서버쪽에서 document 접근 불가능.
-// import { getKeyFromCookie } from "./shared/lib/cookie";
+// import { getCookieByKey } from "./shared/lib/cookie";
 
 export function middleware(req: NextRequest) {
     const user = req.cookies.get("user");
 
+    const url = req.nextUrl;
+
     if (!user) {
-        return NextResponse.redirect(new URL("/", req.url));
+        // 유저가 없는데... "/board"로 접근 시 "/"로 리다이렉트
+        if (url.pathname.startsWith("/board")) {
+            return NextResponse.redirect(new URL("/", req.url));
+        }
+    } else {
+        // 유저가 쿠키에 있고, "/" 또는 "/signup"에 접근시엔 board로
+        if (url.pathname === "/" || url.pathname === "/signup") {
+            return NextResponse.redirect(new URL("/board", req.url));
+        }
     }
 
     return NextResponse.next();
@@ -17,5 +27,5 @@ export function middleware(req: NextRequest) {
 
 // 인증이 필요한 경로 지정
 export const config = {
-    matcher: ["/board"],
+    matcher: ["/", "/signup", "/board/:path*"],
 };
